1d7a6c56aa2fa2cb40d424f4f4efeb9ef8feb2cb
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-10 04:11:41 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,7 @@
+SQL full form is a structured query language, and it allows you to interact through commands with the database. Here are some of the SQL Database functions:
+
+This allows users to retrieve information from the relational database.
+It enables the development of tables and databases.
+It allows data base and tables to be modified, added, removed, and changed.
+It gives protection, and allows permission to be set.
+Allows new ways for people to manage the info.
